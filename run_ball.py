a827415d3e3f0eb19a891c8913d566fdcf8e5469
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2023-11-16 11:23:58 +0700

    Create the ball objects

--- run_ball.py
+++ run_ball.py
@@ -1,26 +1,17 @@
 import turtle
-import ball
+from ball import Ball
 
-num_balls = int(input("Number of balls to simulate: "))
 turtle.speed(0)
 turtle.tracer(0)
 turtle.hideturtle()
-canvas_width = turtle.screensize()[0]
-canvas_height = turtle.screensize()[1]
-ball_radius = 0.05 * canvas_width
 turtle.colormode(255)
-color_list = []
-xpos = []
-ypos = []
-vx = []
-vy = []
-ball_color = []
-ball.initilizing(xpos, ypos, vx, vy, ball_color, canvas_width, canvas_height, ball_radius, num_balls)
+ball = Ball()
+ball.initilizing()
 while (True):
     turtle.clear()
-    for i in range(num_balls):
-        ball.draw_circle(ball_color[i], ball_radius, xpos[i], ypos[i])
-        ball.move_circle(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius)
+    for i in range(ball.num_balls):
+        ball.draw_circle(i)
+        ball.move_circle(i)
     turtle.update()
 
 # hold the window; close it by clicking the window close 'x' mark
